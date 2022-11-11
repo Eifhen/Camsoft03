1bae792ca698b9bbdcce09fc2af8d1377795f0c7
Gabriel Jiménez <Jimenezg905@gmail.com>
2022-11-10 21:00:35 -0400

    Delete UI/app/src directory

--- UI/app/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './css/bundle.css';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <><h1>hola mundo</h1></>
-)
